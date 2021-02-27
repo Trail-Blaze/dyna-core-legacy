f3ea2d8223e55f77fa1df994244d31a9eace6cd5
Immanuel Garcia <34188635+AlexDev404@users.noreply.github.com>
2021-02-27 15:42:52 -0600

    Unfinished app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express"); // Used to activate the NodeJs express application libraries
+const net = require("net"); // Used to listen for an empty random port
+const fs = require("fs"); // Used to access the immediate filesystem of an application.
+const { v4: uuidv4 } = require('uuid'); // Used to generate a unique random ID
+// Insert error lib here
+const { console } = 
+uuidv4(); // -> '6c84fb90-12c4-11e1-840d-7b25c5ee775a' 
+
+var srv = net.createServer(function(sock) {
+  sock.end('Hello world\n');
+});
+srv.listen(0, function() {
+  console.log('Listening on port ' + srv.address().port);
+});
