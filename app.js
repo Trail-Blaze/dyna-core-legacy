ec585754fd628d6767f5d94cba95e6e9539402fb
Immanuel Garcia <34188635+AlexDev404@users.noreply.github.com>
2021-03-01 09:04:25 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Definitions
 
 const express = require("express"); // Used to activate the NodeJs express application libraries
-const net = require("net"); // Used to listen for an empty random port
+const getPort = require("get-port"); // Used to listen for an empty random port
 const fs = require("fs"); // Used to access the immediate filesystem of an application.
 const { v4: uuidv4 } = require('uuid'); // Used to generate a unique random ID
 // Insert error lib here
@@ -11,15 +11,7 @@ const REQ_LOGGING = false; // Request Logging is set to false by default
 
 // uuidv4(); // -> '6c84fb90-12c4-11e1-840d-7b25c5ee775a' 
 
-var srv = net.createServer(function(sock) {
-  sock.end('Hello world\n');
-});
-srv.listen(0, function() {
-  console.log('Listening on port ' + srv.address().port);
-});
-
-
-(function () { // Main Function
+(async () => { // Main Function
   // https://www.w3schools.com/js/js_strict.asp
   
  "use strict"; // Prevents program from using uninitialized and undeclared variables
@@ -36,6 +28,17 @@ srv.listen(0, function() {
 }
   // https://expressjs.com/en/starter/static-files.html
   
-  app.use("/", express.static('public')); // Set serverdir to rootdir and create an HTTP server that serves static files
-  
-}());
+  app.use("/", express.static("public")); // Set serverdir to rootdir and create an HTTP server that serves static files
+  app.use((req, res, next) => { // req and res are never used in this scope
+      next(new ApiException(errors.com.epicgames.common.not_found)); // IDK
+  });
+
+  // https://github.com/sindresorhus/get-port/
+  // https://www.npmjs.com/package/get-port
+
+  const port = await getPort(); // Dynamically fetches a random port
+  app.listen(port, () => {
+      console.log("")
+  })
+	
+});
